89ced645df350f5ca006c1f1a647f3b68932a966
WMW <wnameless@gmail.com>
2014-03-09 12:24:30 -0400

    Add comprasions

--- src/main/java/com/wmw/guava/example/GuavaExample.java
+++ src/main/java/com/wmw/guava/example/GuavaExample.java
@@ -6,6 +6,9 @@ import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Maps.newHashMap;
 import static com.google.common.collect.Sets.newHashSet;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -19,37 +22,77 @@ import com.google.common.collect.Multimap;
 
 public class GuavaExample implements Comparable<GuavaExample> {
 
-  int i;
-  double d;
+  Integer i;
+  Double d;
 
   @SuppressWarnings("unused")
   public void staticFactoryMethodsOfCollections() {
+    // Without Guava
+    List<Integer> list1 = new ArrayList<Integer>();
+    Set<String> set1 = new HashSet<String>();
+    Map<Integer, String> map1 = new HashMap<Integer, String>();
+
+    // With Guava
+    List<Integer> list2 = newArrayList();
+    Set<String> set2 = newHashSet();
+    Map<Integer, String> map2 = newHashMap();
     // Don't need to retype the generic anymore
-    List<Integer> list = newArrayList();
-    Set<String> set = newHashSet();
-    Map<Integer, String> maop = newHashMap();
   }
 
-  public void equalMethodForFindingTheEquilityOfAny2Objects() {
-    // No NullPointerException anymore
-    Objects.equal(null, "ABC");
-    // It's often used to override the equals() method of a class
+  @SuppressWarnings("unused")
+  @Override
+  public boolean equals(Object o) {
+    if (o instanceof GuavaExample) {
+      GuavaExample ge = (GuavaExample) o;
+      // Without Guava
+      boolean b =
+          (i == null ? ge.i == null : i.equals(ge.i))
+              && (d == null ? ge.d == null : d.equals(ge.d));
+      // return b;
+
+      // With Guava
+      return Objects.equal(i, ge.i) && Objects.equal(d, ge.d);
+      // No need to handle NullPointerException anymore
+    }
+    return false;
   }
 
-  public void friendlyHashCodeMethod() {
-    // It's often used to override the hashCode() method of a class
-    Objects.hashCode("a", 1, 1.0);
+  @Override
+  public int hashCode() {
+    // Without Guava
+    int result = 6;
+    result = result ^ 31 + i.hashCode();
+    result = result ^ 31 + d.hashCode();
+    // return result;
+
+    // With Guava
+    return Objects.hashCode(i, d);
   }
 
-  public void dummyToStringImplementation() {
-    // It can easily print out all properties of an object
-    Objects.toStringHelper(getClass()).add("int", i).add("double", d)
+  @SuppressWarnings("unused")
+  @Override
+  public String toString() {
+    // Without Guava
+    String s =
+        getClass().getSimpleName() + "{int=" + "i, " + "double=" + d + "}";
+    // return s;
+
+    // With Guava
+    return Objects.toStringHelper(getClass()).add("int", i).add("double", d)
         .toString();
   }
 
   @SuppressWarnings("unused")
   public void cleanStaticMethodsToEnforceProconditions(String str) {
-    String s = checkNotNull(str, "Argument str can't be null.");
+    // Without Guava
+    if (str == null)
+      throw new NullPointerException("Argument str can't be null.");
+    if (str.isEmpty())
+      throw new IllegalArgumentException("Argument str can't be empty.");
+    String s = str;
+
+    // With Guava
+    s = checkNotNull(str, "Argument str can't be null.");
     checkArgument(!str.isEmpty(), "Argument str can't be empty");
   }
 
@@ -57,6 +100,7 @@ public class GuavaExample implements Comparable<GuavaExample> {
   public void usefulMultiMap() {
     // Instead of doing something like this
     Map<Integer, List<String>> map = newHashMap();
+
     // Try to do it with the new Multimap collection provided by Guava
     Multimap<Integer, String> mmap = ArrayListMultimap.create();
   }
@@ -66,13 +110,24 @@ public class GuavaExample implements Comparable<GuavaExample> {
     // Instead of doing something like this
     Map<Integer, String> int2Str = newHashMap();
     Map<String, Integer> str2Int = newHashMap();
+
     // Try to do it with the new BiMap collection provided by Guava
     BiMap<Integer, String> biMap = HashBiMap.create();
   }
 
-  // Smart ComparisonChain makes you override compareTo much easier
   @Override
   public int compareTo(GuavaExample o) {
+    // Without Guava
+    int result;
+    result = i.compareTo(o.i);
+    if (result != 0)
+      return result;
+
+    result = d.compareTo(o.d);
+    if (result != 0)
+      return result;
+
+    // With Guava
     return ComparisonChain.start().compare(i, o.i).compare(d, o.d).result();
   }
 
