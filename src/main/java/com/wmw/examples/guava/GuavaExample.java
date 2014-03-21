ba57d653055d9b46700a6dc093897d15b5fadabe
WMW <wnameless@gmail.com>
2014-03-21 11:45:43 -0400

    Add immutable collections with Guava examples

--- src/main/java/com/wmw/examples/guava/GuavaExample.java
+++ src/main/java/com/wmw/examples/guava/GuavaExample.java
@@ -25,6 +25,8 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Maps.newHashMap;
 import static com.google.common.collect.Sets.newHashSet;
+import static java.util.Collections.unmodifiableList;
+import static java.util.Collections.unmodifiableMap;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -39,6 +41,8 @@ import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.ComparisonChain;
 import com.google.common.collect.HashBiMap;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Multimap;
 
 public class GuavaExample implements Comparable<GuavaExample> {
@@ -48,22 +52,40 @@ public class GuavaExample implements Comparable<GuavaExample> {
 
   @SuppressWarnings("unused")
   public void staticFactoryMethodsOfCollections() {
-    // Without Guava
+    // Generic collections without Guava
     List<Integer> list1 = new ArrayList<Integer>();
     Set<String> set1 = new HashSet<String>();
     Map<Integer, String> map1 = new HashMap<Integer, String>();
 
-    // With Guava
+    // Generic collections with Guava
     List<Integer> list2 = newArrayList();
     Set<String> set2 = newHashSet();
     Map<Integer, String> map2 = newHashMap();
     // Don't need to retype the generic anymore
 
-    // Without Guava
+    // Prepopulated collections without Guava
     List<Integer> ints1 = new ArrayList<Integer>(Arrays.asList(1, 2, 3));
 
-    // With Guava
+    // Prepopulated collections with Guava
     List<Integer> ints2 = newArrayList(1, 2, 3);
+
+    // Immutable collections without Guava
+    List<String> strs1 =
+        unmodifiableList(new ArrayList<String>(Arrays.asList("A", "B", "C")));
+    @SuppressWarnings("serial")
+    Map<Integer, String> int2Str1 =
+        unmodifiableMap(new HashMap<Integer, String>() {
+
+          {
+            put(1, "a");
+            put(2, "b");
+            put(3, "c");
+          }
+        });
+
+    // Immutable collections with Guava
+    List<String> strs2 = ImmutableList.of("A", "B", "C");
+    Map<Integer, String> int2Str2 = ImmutableMap.of(1, "a", 2, "b", 3, "c");
   }
 
   @SuppressWarnings("unused")
